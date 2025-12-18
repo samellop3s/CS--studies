de0016360f0c6e083321132e854da80460c67048
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2025-12-18 20:36:51 -0300

    test

---
+++ Aula01/Aula01.cs
@@ -0,0 +1,9 @@
+using System; //Serve para usar uma bibloteca padrão do C#
+
+class Principal
+{
+    static void Main()
+    {
+        Console.Write("Olá, Mundo!"); //Escreve na tela a mensagem "Olá, Mundo!"
+    }
+}
