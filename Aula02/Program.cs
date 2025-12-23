4d8f61f6147a88641c9fceff386c7d5b1cc1ffff
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2025-12-22 21:08:46 -0300

    Aula02 - testando parametros

---
+++ Aula02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Aula02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eu sou o seu primeiro programa em C#!");
+            if(args.GetLength(0) > 0)
+            {
+                Console.WriteLine(args.GetValue(0));
+            }else
+            {
+                Console.WriteLine("Nenhum argumento foi passado.");
+            }
+        }
+    }
+}
