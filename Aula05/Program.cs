8992a590eaac6ee110b8080737d12bcd86ecc53f
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2025-12-23 23:19:59 -0300

    Aula05 - operadores e operação - finalizado

---
+++ Aula05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main()
+    {
+       /*bool res=10<5;
+       int num = 10;
+       num++;
+       //num+=1;
+
+
+       Console.WriteLine(res);*/
+
+        //& = AND / E
+        //| = OR / OU       
+        bool resul=(5>3)&(10<2);
+        Console.WriteLine(resul);
+    }
+}
