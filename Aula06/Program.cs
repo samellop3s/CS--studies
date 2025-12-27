fdda3c619471012311bff780dff5aba99d0f26c9
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2025-12-27 15:06:57 -0300

    aula06 - formatando a saida no console

--- Aula06/Program.cs
+++ Aula06/Program.cs
@@ -18,7 +18,25 @@ class Program
         n1 = 10;
         n2 = 20;
         n3 = 30;
+        //o \n ele serve para fazer uma quebra de linha no console.
+        //o \t ele serve para dar um tab no console.
+        Console.WriteLine("\nn1=\t{0}\nn2=\t{1}\nn3=\t{2}\n", n1, n2, n3);
 
-        Console.WriteLine("n1={0}, n2={1}, n3={2}", n1, n2, n3);
+        double valorCompra = 5.50;
+        double valorvenda;
+        double lucro = 0.20;
+        string produto = "Coxinha";
+        
+        valorvenda = valorCompra + (valorCompra * lucro);
+
+        Console.WriteLine("Produto:.........{0,15}", produto);
+        Console.WriteLine("Val. Compra:.....{0,15:c}", valorCompra);
+        Console.WriteLine("Val. Venda:......{0,15:c}", valorvenda);
+        Console.WriteLine("Lucro:...........{0,15:p}", lucro);
+
+        //0 - indice do valor
+        //,15 - espaço reservado para o valor (15 caracteres)
+        //:c - formatação do valor (moeda)
+        //:p - formatação do valor (porcentagem)
     }
 }
