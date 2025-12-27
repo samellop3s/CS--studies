5d6c285e5960120b089db4aa02890ffb299fee99
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2025-12-27 15:55:12 -0300

    aula07 constantes em C# - finalizada

--- Aula07/Program.cs
+++ Aula07/Program.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Ola Mundo!");
+        //const seria uma variável imutável.
+        const string name = "Samuel Ferreira Lopes";
+        const int year = 21;
+        const double pi = 3.14;
+
+        Console.WriteLine("Constantes:");
+        Console.WriteLine($"Nome: {name}");
+        Console.WriteLine($"Idade: {year}");
+        Console.WriteLine($"Pi: {pi}");
     }
 }
