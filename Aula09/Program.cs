56e8ab2e18f66151161a46d85eb7d646641fcc62
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-01-06 21:03:47 -0300

    Aula09 - bitwise

--- Aula09/Program.cs
+++ Aula09/Program.cs
@@ -6,7 +6,16 @@ namespace Aula09
     {
         static void Main(string[] args)
         {
+            //Bitwise é uma operação de baixo nível que manipula bits individuais, 
+            //ou padrões de bits, em números binários. É um tipo de computação direta com representações binárias de dados
             
+            //<< Deslocamento para a esquerda dos bits dobra o valor a cada deslogamento    
+            //>> Deslocamento para a direita dos bits reduz o valor pela metade a cada deslocamento
+
+            int num = 10; 
+
+            num = num << 2; // Deslocamento para a esquerda em 2 bits (equivale a multiplicar por 4)
+            Console.WriteLine(num); 
         }
     }
 }
