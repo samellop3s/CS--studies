7287f6facd01f6dc1df8bc8a3945cb3fd8413561
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-01-06 21:20:35 -0300

    Aula10 - Enum

---
+++ Aula10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Aula09
+{
+    class Program
+    {
+        enum DiasSemana { Domingo, Segunda, Terca, Quarta, Quinta, Sexta, Sabado };// Definição do enum DiasSemana 
+        static void Main(string[] args)
+        {
+            DiasSemana dia = DiasSemana.Quarta; // Atribuição de um valor do enum a uma variável
+            Console.WriteLine("Dia da semana: " + dia);
+
+            DiasSemana outroDia = (DiasSemana)5; // Conversão de um valor inteiro para o enum
+            Console.WriteLine("Outro dia da semana: " + outroDia);
+        }
+    }
+}
