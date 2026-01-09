05c3af42202ce4cd9dff862a2f692124b12f2d11
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-01-08 22:21:35 -0300

    fazendo manipulação de dados especiais (ENUM)

--- Aula10/Program.cs
+++ Aula10/Program.cs
@@ -4,6 +4,10 @@ namespace Aula09
 {
     class Program
     {
+
+        // O ENUM (enumeração) é um tipo de dado especial que permite definir um conjunto de constantes nomeadas.
+        // Ele é usado para representar um grupo de valores relacionados de forma mais legível e organizada
+
         enum DiasSemana { Domingo, Segunda, Terca, Quarta, Quinta, Sexta, Sabado };// Definição do enum DiasSemana 
         static void Main(string[] args)
         {
@@ -12,6 +16,15 @@ namespace Aula09
 
             DiasSemana outroDia = (DiasSemana)5; // Conversão de um valor inteiro para o enum
             Console.WriteLine("Outro dia da semana: " + outroDia);
+
+            //O Enum.Parse é um método que converte uma string em um valor do enum correspondente.
+            DiasSemana qualquerDia = Enum.Parse<DiasSemana>("Domingo"); // Conversão de string para o enum
+            Console.WriteLine("Qualquer dia da semana: " + qualquerDia);
+
+            Console.Write("Qual será o dia q vai cortar o seu cabelo? ");
+            string entrada = Console.ReadLine(); //recebera o valor dado pelo usuario
+            DiasSemana corte = Enum.Parse<DiasSemana>(entrada); //fara a conversao do valor recebido para o enum
+            Console.WriteLine("Seu corte foi marcado pro dia: " + corte); //mostrara o valor convertido
         }
     }
 }
