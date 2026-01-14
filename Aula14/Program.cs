fdc406fcbe0ba752b069907b1f5c21699e7511ee
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-01-14 18:20:58 -0300

    if aninhado

---
+++ Aula14/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Aula12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Condicional if-else
+
+            int n1, n2, n3, n4, res;
+            res = n1 = n2 = n3 = n4 = 0;
+
+            //>= 60 Aprovado
+            //50=< 60 Recuperação
+            //< 50 Reprovado
+
+            Console.Write("Digite o valor de n1: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            if (n1 >= 60)
+            { if(n1 >= 90)
+                {
+                    Console.WriteLine("Aprovado com louvor");
+                }else
+                {
+                    Console.WriteLine("Aprovado");
+                }
+            }
+            else
+            {
+                if(n1 >= 50)
+                {
+                    Console.WriteLine("Recuperação");
+                }else
+                {
+                    Console.WriteLine("Reprovado");
+                }
+            }
+        }
+    }
+}   
