eaec2fd14666eea3f2c47a1cdec1cc5284cdd6ba
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-01-13 22:19:20 -0300

    Comando condicional com if/else

---
+++ Aula12/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aula12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Comando condicional com if/else
+            Console.Write("Digite a nota do aluno: ");
+            int nota = int.Parse(Console.ReadLine());
+
+            if(nota >= 60)
+            {//se o resultado da condição for verdadeiro
+                Console.WriteLine("Aprovado");
+            }
+            else
+            {//se o resultado da condição for falso
+                Console.WriteLine("Reprovado");
+            }
+        }
+    }
+}
