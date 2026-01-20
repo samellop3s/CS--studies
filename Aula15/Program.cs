1a477637da7ab8409a5f47ef85c128cf7064cfb6
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-01-20 09:12:53 -0300

    switch case

--- Aula15/Program.cs
+++ Aula15/Program.cs
@@ -6,7 +6,26 @@ namespace Aula15
     {
         static void Main(string[] args)
         {
-            
+            //o SWITCH CASE é uma estrtura condicional que permite testar uma variável
+            //contra vários valores possíveis, executando o bloco de código correspondente
+
+            Console.WriteLine("Escolha o local de viagem:");
+            Console.WriteLine("1 - Praia || 2 - Montanha || 3 - Campo || 4 - Cidade || 5 - Interior");
+            Console.Write("Digite o número correspondente ao local desejado:");
+            int escolha = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escolha o modo de transporte: (a) Carro || (b) Avião || (c) Trem || (d) Ônibus");
+            Console.Write("Digita a letra correspondente ao modo de transporte desejado:");
+            char transporte = char.Parse(Console.ReadLine());
+
+            switch (escolha)
+            {
+                
+            }
+
+            switch (transporte)
+            {
+                
+            }
         }
     }
 }
