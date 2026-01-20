311987b74fd6d5875cb88bff545f9f2f7ea28248
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-01-20 19:10:06 -0300

    aula17 entendendo arrays e vetores

--- Aula17/Program.cs
+++ Aula17/Program.cs
@@ -8,7 +8,22 @@ namespace Aula17
         {
 
             //Arrays / vetores
+            int [] n = new int[5];
+
+            int[] num = new int[3]{55, 65, 75};
+
+            //sempre lembrar que o primeiro elemento do array começa no índice 0
+            //então um array de 5 posições vai do índice 0 ao índice 4
+            //atribuindo valores ao array
             
+
+            n[0] = 10;
+            n[1] = 20;
+            n[2] = 30;
+            n[3] = 40;
+            n[4] = 50;
+            
+            Console.WriteLine(num[2]);
             
         }
     }
