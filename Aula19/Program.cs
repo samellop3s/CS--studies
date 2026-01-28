985b5a34b4817e35657c97c89f9f49f12928611d
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-01-27 21:37:04 -0300

    att repositorio

--- Aula19/Program.cs
+++ Aula19/Program.cs
@@ -13,6 +13,48 @@ namespace Aula19
                 Console.WriteLine("CFB cursos");
                 break;//comando para sair do loop
             }
+
+            Console.Write("==============================\n");
+
+            int [] numbers = {10,20,30,40,50,60,70,80,90,100};
+            Console.Write("\nImprimindo os valores do Array\n");
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            Console. Write("\nImprimindo os valores do Array ao contrario\n");
+            for(int i = numbers.Length -1; i>=0; i--)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            
+            Console.WriteLine();
+            Console.Write("==============================\n");
+
+            string [] carros = {"BMW", "AUDI", "FERRARI", "MERCEDES", "VOLVO"};
+
+            bool encontrou = false;
+
+            foreach(string carro in carros)
+            {
+                if( carro == "FERRARI" )
+                {
+                    encontrou = true;
+                    break;
+                }
+            }if (encontrou)
+            {
+                Console.WriteLine("Carro encontrado!");
+            }else
+            {
+                Console.WriteLine("Carro nao encontrado!");
+            }
+
+            Console.Write("==============================\n");
         }
     }
 }
