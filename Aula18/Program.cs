c66bbe1bed44857fd0727f897ff459edb632ba7a
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-02-11 22:49:31 -0300

    att repo

--- Aula18/Program.cs
+++ Aula18/Program.cs
@@ -7,40 +7,17 @@ namespace Aula18
         static void Main(string[] args)
         {
             //nesta aula foi passado como funciona a matrix com 2 vetores ou mais
+            int[,] n = new int [3,5]; //aqui estou indicando que sera uma matrix bidimensional, onde vai receber 3 linhas para 5 colunas
 
-            int[,] n = new int[3, 6];
+            //aqui estou passando o valor de cada vetor
+            n[0,0] = 10; n[1,0] = 60; n[2,0] = 25;
+            n[0,1] = 20; n[1,1] = 70; n[2,1] = 35;
+            n[0,2] = 30; n[1,2] = 80; n[2,2] = 45;
+            n[0,3] = 40; n[1,3] = 90; n[2,3] = 55;
+            n[0,4] = 50; n[1,4] = 15; n[2,4] = 65;
 
+            Console.WriteLine(n[1,3]);
 
-            /*
-             n[0,0] = 10;
-             n[0,1] = 20;
-             n[0,2] = 20;
-             n[0,3] = 20;
-             n[0,4] = 20;
-             n[0,5] = 20;
-
-             n[1,0] = 30;
-             n[1,1] = 40;
-             n[1,2] = 40;
-             n[1,3] = 40;
-             n[1,4] = 40;
-             n[1,5] = 40;
-
-             n[2,0] = 50;
-             n[2,1] = 60;
-             n[2,2] = 60;
-             n[2,3] = 60;
-             n[2,4] = 60;
-             n[2,5] = 60;
-            */
-
-            //outra forma de atribuir valores a matrix
-            n[0, 0] = 10; n[0, 1] = 20; n[0, 2] = 20; n[0, 3] = 20; n[0, 4] = 20; n[0, 5] = 20;
-            n[1, 0] = 30; n[1, 1] = 40; n[1, 2] = 40; n[1, 3] = 40; n[1, 4] = 40; n[1, 5] = 40;
-            n[2, 0] = 50; n[2, 1] = 60; n[2, 2] = 60; n[2, 3] = 60; n[2, 4] = 60; n[2, 5] = 60;
-
-            //acessando um valor da matrix
-            Console.WriteLine(n[2, 3]);
 
         }
     }
