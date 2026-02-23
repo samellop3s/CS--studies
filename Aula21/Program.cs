cf0dc6bf8f202ea8e50af5d23e54829d196c0093
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-02-23 20:57:42 -0300

    aula21 finalizada

--- Aula21/Program.cs
+++ Aula21/Program.cs
@@ -1,4 +1,4 @@
-﻿using system;
+﻿using System;
 
 namespace AppTest
 {
@@ -6,7 +6,30 @@ namespace AppTest
     {
         static void Main(string[] args)
         {
-            //
+            //nesta aula vms aprender sobre loop do while
+            //primeira diferença entre o while e do while é a sua sintaxe 
+
+           /* while(cond)//o while ele primeiro testa e dps executa
+            {
+                
+            }*/
+
+            //o do while, executa a função pelo menos uma unica vez
+            //ja o while nao faz essa execução
+
+            string senha = "123";
+            string senhaUser;
+            int tentativas=0;
+
+            do{//ja o dowhile ele executa primeiro e dps testa
+                Console.Clear();
+                Console.WriteLine("Digite sua senha: ");
+                senhaUser = Console.ReadLine();
+                tentativas++;
+            } while( senha != senhaUser);
+
+            Console.Clear();
+            Console.WriteLine("Senha correta, tentativas:{0}", tentativas);
         }
     }
 }
