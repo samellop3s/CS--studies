fe1e0e636ad6e05252285e0276e2cbab8033de8f
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-02-23 20:37:31 -0300

    aula20 finalizada

--- Aula20/Program.cs
+++ Aula20/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace AppTest
 {
@@ -6,15 +7,33 @@ namespace AppTest
     {
         static void Main(string[] args)
         {
-            int[] number = new int[10];
-            int i = 0;
+
+            //o loop for serve quando sabemos a quantidade de valores q desejamos.
+            //ja o loop while, serve quando n sabemos a quantidade dos valores.
+
+
+           /* int[] number = new int[10];
+
+            int  i=0;//no loop while sempre passar o valor fora antes de iniciar a func
             while (i<number.Length)
             {
-                number[i]=0;
-                Console.WriteLine(number[i]);
-                i++;
+                number[i] = 0;
+                Console.WriteLine(number[1]);
+                i++;//semopre verificar se tem a acao de incremento ou decremento
             }
-            Console.WriteLine("Fim do loop");
+
+            Console.WriteLine("Fim do loop");*/
+
+            int[] num = new int[10];
+
+            int i=num.Length -1;
+            while (i >0)
+            {
+                num[i] = 0;
+                Console.WriteLine(num[i]);
+                i--;
+            }
+            Console.WriteLine("finalizado");
         }
     }
-}
+} 
