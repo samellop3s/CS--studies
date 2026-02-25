0006d2bbfae430a456c7235002b11319106ef916
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-02-25 20:22:00 -0300

    pausa

--- Aula20/Program.cs
+++ Aula20/Program.cs
@@ -12,17 +12,17 @@ namespace AppTest
             //ja o  while, serve quando n sabemos a quantidade dos valores.
 
 
-           /* int[] number = new int[10];
+            /* int[] number = new int[10];
 
-            int  i=0;//no loop while sempre passar o valor fora antes de iniciar a func
-            while (i<number.Length)
-            {
-                number[i] = 0;
-                Console.WriteLine(number[1]);
-                i++;//semopre verificar se tem a acao de incremento ou decremento
-            }
+             int  i=0;//no loop while sempre passar o valor fora antes de iniciar a func
+             while (i<number.Length)
+             {
+                 number[i] = 0;
+                 Console.WriteLine(number[1]);
+                 i++;//semopre verificar se tem a acao de incremento ou decremento
+             }
 
-            Console.WriteLine("Fim do loop");*/
+             Console.WriteLine("Fim do loop");*/
 
             /*int[] num = new int[10];
 
@@ -35,18 +35,35 @@ namespace AppTest
             }
             Console.WriteLine("finalizado");*/
 
-            int [] number = new int [10]; //estamos informando a quantidade q deve ser repetida
+            /* int [] number = new int [10]; //estamos informando a quantidade q deve ser repetida
 
-            int i = 0;//informando q o i sera iniciado em 0
+             int i = 0;//informando q o i sera iniciado em 0
 
-            while(i<number.Length)
+             while(i<number.Length)
+             {
+                 number[i] = 0;
+                 Console.WriteLine("Tst");
+                 i++;//após o teste da condição e retornar corretamente será somado +1
+             }
+
+             Console.WriteLine("ação finalizada");*/
+
+            int[] number = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            for (int i = 0; i < number.Length; i++)
             {
-                number[i] = 0;
-                Console.WriteLine("Tst");
-                i++;//após o teste da condição e retornar corretamente será somado +1
+                Console.Write("Valor dos vetores: ");
+                Console.WriteLine(number[i]);
             }
 
-            Console.WriteLine("ação finalizada");
+            Console.WriteLine();
+
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                Console.Write("valores invertidos: ");
+                Console.WriteLine(number[i]);
+            }
+
         }
     }
-} 
+}
