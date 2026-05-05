8865b544472369dcc6f762214d97dbf1d7395c94
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-05-05 15:21:49 -0300

    retomando alguns pontos sobre metodos

--- Aula24/Program.cs
+++ Aula24/Program.cs
@@ -1,33 +1,24 @@
 ﻿using System;
-using System.ComponentModel;
+using System.Runtime.Intrinsics;
 
-namespace Test
+namespace Aula24
 {
-    class Aula24
+    class AppTest
     {
-       /* static void Main()
-        {
-            Numbers(10, 36); //este metodo esta recebendo os valores do numero
-            //proximo metodo a ser testado sera com valores direto do teclado.
-        }
-
-        static void Numbers(int n1, int n2)
-        {
-            int res = (n1 + n2);
-            Console.Write("A soma de {0} + {1} é igual a {2}", n1, n2, res);//aqui sera impresso tanto a mensagem quanto o valor da soma
-        }*/
-
         static void Main()
-        { 
-            int v1 = Convert.ToInt32(Console.ReadLine());//aqui será a entrada dos valores vindo direto do teclado e sendo convertido.
-            int v2 = Convert.ToInt32(Console.ReadLine());
-            Numbers(v1, v2);//aqui esta sendo chamada as variaveis.
+        {   
+            Console.WriteLine("Digitar valor do number1: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digitar valor do number2: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int soma = Soma (number1, number2);
         }
 
-        static void Numbers(int v1, int v2)
+        static int Soma( int number1, int number2)
         {
-            int result = v1 + v2;
-            Console.WriteLine($"A soma entre {v1} + {v2} é igual a {result}");
+            int res = number1 + number2;
+            Console.WriteLine($"A soma entre {number1} + {number2} é igual a {res}");
+            return res;
         }
     }
 }
