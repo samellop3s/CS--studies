fb28af0f6698591c4cb11e428eff8478b401a12e
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-05-05 15:47:31 -0300

    teste

--- Aula24/Program.cs
+++ Aula24/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Runtime.Intrinsics;
 
 namespace Aula24
@@ -6,19 +7,22 @@ namespace Aula24
     class AppTest
     {
         static void Main()
-        {   
-            Console.WriteLine("Digitar valor do number1: ");
-            int number1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Digitar valor do number2: ");
-            int number2 = Convert.ToInt32(Console.ReadLine());
-            int soma = Soma (number1, number2);
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            while (number > 0)
+            {
+                int digitado = number % 10;
+                Console.WriteLine(digitado);
+                number /= 10;
+            }
         }
 
-        static int Soma( int number1, int number2)
+      /*  static int Soma( int number1, int number2)
         {
             int res = number1 + number2;
             Console.WriteLine($"A soma entre {number1} + {number2} é igual a {res}");
             return res;
-        }
+        } */
     }
 }
