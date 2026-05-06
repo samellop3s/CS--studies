3e5e74d1afa248007440b347177f56957054faa0
samuelferreiralopes <samuelferreiralopes6@gmail.com>
2026-05-05 22:31:44 -0300

    teste de logica

--- Aula24/Program.cs
+++ Aula24/Program.cs
@@ -1,35 +1,17 @@
 ﻿using System;
-using System.ComponentModel;
-using System.Globalization;
-using System.Runtime.InteropServices;
-
-namespace Test
+class Program
 {
-    class Aula24
+    static void Main()
     {
-       /* static void Main()
-        {
-            Numbers(10, 36); //este metodo esta recebendo os valores do numero
-            //proximo metodo a ser testado sera com valores direto do teclado.
-        }
+        Console.WriteLine("Digite o primeiro numero: ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite o primeiro numero: ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
 
-        static void Numbers(int n1, int n2)
-        {
-            int res = (n1 + n2);
-            Console.Write("A soma de {0} + {1} é igual a {2}", n1, n2, res);//aqui sera impresso tanto a mensagem quanto o valor da soma
-        }*/
-
-        static void Main()
-        {
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            Numbers(num1, num2);
-        }
-
-        static void Numbers(int num1, int num2)
-        {
-            int soma = num1 + num2;
-            Console.WriteLine($"O resultado da soma entre {0} + {1} é igual a {2}", num1, num2, soma);
-        }
+        Console.WriteLine($"{number1} + {number2} = {Soma(number1, number2)}");
+    }
+    static int Soma(int number1, int number2)
+    {
+        return number1 + number2;
     }
 }
