c703cb313a02c43bc6bb47b690eed34fc8ce5330
RafaelFLopes <ferreiralopesrafael109@gmail.com>
2026-05-20 21:55:21 -0300

    atividade de teste de aprendizado

--- TestAulas/Program.cs
+++ TestAulas/Program.cs
@@ -1,42 +1,45 @@
 ﻿using System;
 
-namespace TestAulas
-{
-    class Program
+class Program {
+    static void Main()
     {
-        static void Main(string[] args)
+        Console.WriteLine("Qual fruta o chico gosta: ");
+        String fruta = Console.ReadLine();
+
+        Console.WriteLine($"Quantas {fruta} o chico comeu pela manhã: ");
+        double number1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"Quantas {fruta} o chico comeu no horario do almoço: ");
+        double number2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"Quantas {fruta} o chico comeu na janta: ");
+        double number3 = Convert.ToDouble(Console.ReadLine());
+
+        double alimento = QuantidadeDeGoiaba(number1, number2, number3);
+
+        string situacao = SituacaoDoChico(alimento);
+
+        Console.WriteLine($"O chico gosta de:{fruta} \nqual foi a quantidade que ele comeu ao longo do dia?: {alimento} \nqual a situação do chico?: {situacao}");
+
+    }
+
+    static double QuantidadeDeGoiaba(double number1, double number2, double number3)
+    {
+        return (number1 + number2 + number3);
+    }
+
+    static string SituacaoDoChico(double alimento)
+    {
+        if (alimento <= 6)
         {
-            int codigo, quantidade;
-            double lucro, valor;
-            string produto;
-
-
-            Console.WriteLine("---LK IMPORTADORA---");
-
-            Console.Write("Digite o código do produto: ");
-            codigo = int.Parse(Console.ReadLine());
-            Console.Write("Digite o nome do produto: ");
-            produto = Console.ReadLine();
-            Console.Write("Digite o valor de compra: ");
-            valor = double.Parse(Console.ReadLine());
-            Console.Write("Digite a quantidade comprada: ");
-            quantidade = int.Parse(Console.ReadLine());
-            Console.Write("Digite o lucro desejado (em decimal): ");
-            lucro = double.Parse(Console.ReadLine());
-            double valorVenda = valor + (valor * lucro);
-
-
-            Console.WriteLine("\n--- NOTA FISCAL ---");
-            Console.WriteLine("Código do produto.......:{0,15}", codigo);
-            Console.WriteLine("Nome do produto.........:{0,15}", produto);
-            Console.WriteLine("Valor de compra.........:{0,15:c}", valor);
-            Console.WriteLine("Quantidade comprada.....:{0,15}", quantidade);
-            Console.WriteLine("Lucro desejado..........:{0,15:p}", lucro);
-            Console.WriteLine("Valor de venda..........:{0,15:c}", valorVenda);
-            Console.WriteLine("Total da compra.........:{0,15:c}", valorVenda * quantidade);
-            Console.WriteLine("-------------------");
-
-
+            return "Chico esta satisfeito";
+        }
+        else if (alimento <= 9)
+        {
+            return "Chico esta sentindo algumas dores na barriga";
+        }
+        else {
+            return "Chico precisa muito de um banheiro!!!!";
         }
     }
 }
