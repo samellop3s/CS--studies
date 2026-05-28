e0e2803c1b8b7690aa469abcbbc1bd5787ac9787
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-05-27 22:19:57 -0300

    validação na entrada de valores correta

--- tstLogica/Program.cs
+++ tstLogica/Program.cs
@@ -10,13 +10,43 @@ namespace TestProgram
             do
             {
                 Console.WriteLine("Whats your name?");
-                name = Console.ReadLine(); // aqui será entregue o valor vindo do teclado
+                name = Console.ReadLine()!; // aqui será entregue o valor vindo do teclado
 
-                if (!ApanesLetra(name));
+                if (!ApenasLetra(name))
                 {
                     Console.WriteLine("ERRO! favor inserir somente letras");
                 }
             }while(!ApenasLetra(name));
+        }
+          /*
+        Método responsável por validar se o texto contém apenas letras.
+
+        foreach:
+        Percorre caractere por caractere da string.
+
+        char.IsLetter(c):
+        Verifica se o caractere atual é uma letra.
+
+        c != ' ':
+        Permite espaços no texto.
+        Exemplo: "maça verde"
+
+        return false:
+        Caso encontre números ou símbolos.
+
+        return true:
+        Caso toda a string seja válida.
+    */
+        static bool ApenasLetra(string texto)
+        {
+            foreach (char c in texto)
+            {
+                if (!char.IsLetter(c) && c != ' ')
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
