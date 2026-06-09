b284855a2e0129986943a24f972f7d68ee511d56
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-06-08 22:15:22 -0300

    relembrando conceitos de logica

--- tstLogica/Program.cs
+++ tstLogica/Program.cs
@@ -11,11 +11,11 @@ namespace TestProgram
             do
             {
                 Console.WriteLine("Favor informar seu nome: ");
-                name = Console.ReadLine();
+                name = Console.ReadLine(); //vai receber o valor desejado pelo teclado
 
-                if (!ApenasLetras(name))
+                if (!ApenasLetras(name)) //logo caira em um if para ser verificador se o valor é valido ou nao, caso seja valido ele ira para o proximo passo, caso seja invalido ele ira mostrar a mensagem de erro e pedir para inserir novamente
                 {
-                    Console.WriteLine("ERRO! favor inserir novamente (apenas letras)");
+                    Console.WriteLine("ERRO! favor inserir novamente (apenas letras)");//mensagem de erro caso o valor seja invalido
                 }
             }while(!ApenasLetras(name));
         }
