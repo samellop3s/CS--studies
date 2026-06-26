29f11344c2691753a6b963f854deaad1ad4f398a
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-06-25 22:39:48 -0300

    criação de calculadora

--- tstLogica/Program.cs
+++ tstLogica/Program.cs
@@ -37,6 +37,44 @@ namespace tstLogica
 
             string resumo = MaiorEntreOsTres(valor1, valor2, valor3);
             Console.WriteLine($"O valor: {resumo}");
+
+            //DESENVOLVER UMA CALCULARDORA QUE SOLICITE 2 NUMEROS
+
+            Console.Write("Digite um valor: " + " ");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite um valor: " + " ");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Selecione uma operação:");
+            Console.WriteLine("1 - soma; \n2 - subtração; \n3 - divisao \n4 - multiplicação");
+            int operacao = Console.ReadLine();
+
+            switch (operacao)
+            {
+                case 1:
+                    resultado = Soma(numero1, numero2);
+                    Console.WriteLine($"{numero1} + {numero2} = {resultado}");
+                break;
+                case 2:
+                    resultado = Subtrair(numero1, numero2);
+                    Console.WriteLine($"{numero1} - {numero2} = {resultado}");
+                break;
+                case 3:
+                    resultado = Divisao(numero1, numero2);
+                    Console.WriteLine($"{numero1} / {numero2} = {resultado}");
+                break;
+                case 4:
+                    resultado = Multiplicar(numero1, numero2);
+                    Console.WriteLine($"{numero1} * {numero2} = {resultado}");
+                break;
+                default :
+                    Console.WriteLine("Opção invalida!");
+                break;
+            }
+
+            /*int sobra = CalcularOperacoes(numero1, numero2);
+            Console.Write($"O resultado da operação entre os numeros{numero1} e {numero2} = {sobra}" + " ");*/
         }
         static int SomaNumbers(int number1, int number2)
         {
@@ -67,6 +105,30 @@ namespace tstLogica
             {
                 return "valor3 é maior";
             }
+        }
+
+        
+
+        static int Soma(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+        static int Subtrair(int numero1, int numero2)
+        {
+            return numero1 - numero2;
+        }
+        static double Divisao(int numero1, int numero2)
+        {
+            if (numero2 == 0)
+            {
+                Console.WriteLine("Não é possivel dividir por zero!");
+                return 0;
+            }
+                return (double)numero1 / numero2;
+        }
+        static int Multiplicar(int numero1, int numero2)
+        {
+            return numero1 * numero2;
         }
     }
 }
