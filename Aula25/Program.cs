3dd7ee68147786e3f8c3866a41a56fea95bdaa1f
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-07-01 22:28:51 -0300

    passagem de argumentos por valores e referencias

--- Aula25/Program.cs
+++ Aula25/Program.cs
@@ -4,9 +4,22 @@ namespace Aula25
 {
     class Program
     {
+        /* Passagem por valor e por referência
+        sempre que ocorrer uma passagem por valor (usa o mesmo argumento passado porem cria um novo local na memoria): vai ser criada uma "copia" mas n ira alterar a "original"
+        agora, se ocorrer uma passagem por referencia (usa o endereço de memoria que foi passada como argumento): sempre que for feita uma alteração, será direta na "original"
+
+        obs: sempre que for passar uma passagem por referencia, use "ref" para a identificar a ação que esta sendo solicitada
+        */
         static void Main()
         {
-            Console.WriteLine("Ola mundo");
+            int num = 500;
+            Dobrar(ref num);
+            Console.WriteLine(num);
+        }
+
+        static void Dobrar(ref int valor)
+        {
+            valor*=2;
         }
     }
 }
